26fee900f041197a03be7a311baeae19cdeae781
Kiryakov Denis <KiryakovDenis@gmail.com>
2025-03-16 20:39:54 +0500

    Initial commit

---
+++ src/main/java/ru/kdv/study/x6notification/x6notificationApp.java
@@ -0,0 +1,11 @@
+package ru.kdv.study.x6notification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class x6notificationApp {
+    public static void main (String[] args) {
+        SpringApplication.run(x6notificationApp.class, args);
+    }
+}
